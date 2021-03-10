e3a8ee1e13256d6606ebae6f5d97c247e508d796
Mikail Masood <mikail.masood@mail.utoronto.ca>
2021-03-10 16:16:39 -0500

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Mikail Masood on 2021-03-10.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
