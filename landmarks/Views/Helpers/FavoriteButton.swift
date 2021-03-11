98d54269742fa6255b4927336d812b0056357fcf
Mikail Masood <mikail.masood@mail.utoronto.ca>
2021-03-10 21:39:23 -0500

    added favorite bool option

---
+++ landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  landmarks
+//
+//  Created by Mikail Masood on 2021-03-10.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button(action: {
+            isSet.toggle()
+        }) {
+            Image(systemName: isSet ? "star.fill" : "star")
+                .foregroundColor(isSet ? Color.yellow : Color.gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
---
+++ landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,38 @@
+//
+//  LandmarkList.swift
+//  landmarks
+//
+//  Created by Mikail Masood on 2021-03-10.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    var body: some View {
+        NavigationView {
+            List{
+                Toggle(isOn: $showFavoritesOnly) {Text("Favorites Only")}
+                ForEach( filteredLandmarks ) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Landmarks")
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
