28956d05a0767cb0be28f7f84c414843f77ec16b
Mikail Masood <mikail.masood@mail.utoronto.ca>
2021-03-11 17:32:13 -0500

    new categories files for featured page

---
+++ landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryRow.swift
+//  landmarks
+//
+//  Created by Mikail Masood on 2021-03-11.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow()
+    }
+}
