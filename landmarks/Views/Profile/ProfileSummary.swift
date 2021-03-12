266a72caeeb04bb1395dd79af42d023f384800dd
Mikail Masood <mikail.masood@mail.utoronto.ca>
2021-03-12 16:16:49 -0500

    screens for profile, editro and profile data

--- landmarks/Views/Profile/ProfileSummary.swift
+++ landmarks/Views/Profile/ProfileSummary.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct ProfileSummary: View {
+    
+    @EnvironmentObject var modelData: ModelData
+    
+    var profile: Profile
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10){
+                Text(profile.username)
+                    .bold()
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                
+                Text("Notifications: \(profile.prefersNotifications ? "On" : "OFF" )")
+                Text(" Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+                
+                VStack (alignment: .leading) {
+                    Text(" Completed Badges ")
+                        .font(.headline)
+                    
+                    ScrollView(.horizontal){
+                        HStack {
+                            HikeBadge(name: "First Hike" )
+                            HikeBadge(name: "Earth Day" ).hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: " Tenth Hike").grayscale(0.5).hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                        
+                    }
+                }
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+                    
+                    HikeView(hike: modelData.hikes[0])
+                }
+            }
+        }
     }
 }
 
 struct ProfileSummary_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileSummary()
+        ProfileSummary(profile: Profile.default)
+            .environmentObject(ModelData())
     }
 }
