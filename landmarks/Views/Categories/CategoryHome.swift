6da58f41eb7269f3b6fa7b24916d4efe7ce34e53
Mikail Masood <mikail.masood@mail.utoronto.ca>
2021-03-12 16:15:46 -0500

    now includes profile link

--- landmarks/Views/Categories/CategoryHome.swift
+++ landmarks/Views/Categories/CategoryHome.swift
@@ -11,6 +11,8 @@ struct CategoryHome: View {
     
     @EnvironmentObject var modelData: ModelData
     
+    @State private var showingProfile = false
+    
     var body: some View {
         NavigationView {
             List {
@@ -26,7 +28,18 @@ struct CategoryHome: View {
                 }
                 .listRowInsets(EdgeInsets())
             }
+            .listStyle(InsetListStyle())
             .navigationTitle("Featured")
+            .toolbar {
+                Button(action: {showingProfile.toggle() }) {
+                    Image(systemName: "person.crop.circle")
+                        .accessibilityLabel("User Profile")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
         }
     }
 }
